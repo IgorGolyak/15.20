95b07ef4a24e1b28e22ea4be778031155f5b9352
Игорь Голяк <golyak.03@inbox.ru>
2021-04-21 15:26:26 +0700

    15

---
+++ Cezst/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cezst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4.104
+            Console.WriteLine("Введите день недельки");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Пнидельник");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Вторник");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Среда");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Четверг");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Пятница");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Суббота");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Воскресенье");
+                        break;
+                    }
+
+
+
+                default:
+                    {
+                        Console.WriteLine("Ошибка данных");
+                        break;
+                    }
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ssd/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите числовое значение оценки");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 2:
+                    {
+                        Console.WriteLine("неуд");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("уд");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("хорошо");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("отлично");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("ошибка ввода данных");
+                        break;
+                    }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
